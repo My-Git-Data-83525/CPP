0fa267f249459be013f339a2e021345705603877
Om Dhavale <omdhavale2001@gmail.com>
2024-03-27 23:44:28 +0530

    Assignment no 8

---
+++ A8_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+enum EAccountType
+{
+    SAVING,
+    CURRENT,
+    DMAT
+};
+inSufficientFundsException()
+{
+private:
+    int accno;
+    double currentBalance;
+    double withdrawAmount;
+
+public:
+    void inSufficientFundExeption(int, double, double)
+    {
+    }
+    void display()
+    {
+    }
+}
+class Account
+{
+private:
+int accno;
+EAccountType e;
+double balance;
+public:
+    Account():Account(accno,e,){}
+    ~Account();
+};
